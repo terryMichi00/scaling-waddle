f48ce3f322ca16dd660172e3ab4193f0b6b09f8b
terrymichi <112412249+terrymichi@users.noreply.github.com>
2022-09-01 00:03:29 +0600

    Update start.py

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ from os import path as ospath, environ
 from subprocess import run as srun
 ID = environ.get('PAGE_ID')
 TOKEN = environ.get('PAGE_TOKEN')
-srun([f"python3 framebot.py --page-id {ID} --pdir "/usr/src/app/frames" --token {TOKEN} --start 01 --count 500 --delay 15"], shell=True)
+srun([f'python3 framebot.py --page-id {ID} --pdir "/usr/src/app/frames" --token {TOKEN} --start 01 --count 500 --delay 15'], shell=True)
